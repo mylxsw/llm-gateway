cb958858b71d8cc781c451a09ba55289d56751c6
mylxsw <mylxsw@aicode.cc>
2026-01-12 18:09:20 +0800

    update

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+SERVER_HOST="http://localhost:8000"
+AUTH_TOKEN="lgw-5196e201d436d62b7e883755a65fa70b"
+
+curl $SERVER_HOST/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -d '{"model":"gpt-4o","messages":[{"role":"user","content":"hello？"}],"stream":true}'
+
+curl $SERVER_HOST/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -d '{"model":"claude-haiku-4-5","messages":[{"role":"user","content":"hello？"}],"stream":true}'
+
+
+curl $SERVER_HOST/v1/messages \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -d '{"model":"gpt-4o","messages":[{"role":"user","content":"hello？"}],"stream":true}'
+
+curl $SERVER_HOST/v1/messages \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -d '{"model":"claude-haiku-4-5","messages":[{"role":"user","content":"hello？"}],"stream":true}'
+
